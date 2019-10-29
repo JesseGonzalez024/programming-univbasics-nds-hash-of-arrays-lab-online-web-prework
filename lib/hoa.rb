b5fa2130b163c80d2ad7c98a44afe71573799889
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-10-29 23:01:46 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,7 +9,9 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
     if BASE_HOA do
-      BASE_HOA[show] << [name]
+        BASE_HOA[show] << [name]
+        show = :chipmunks
+        name = "Dave"
       
     
  
